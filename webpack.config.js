2a134920de4572a83a7e4654c6a1d35ea0fc22d5
kaidu1982 <kaidu1982@gmail.com>
2018-12-24 10:32:38 +0900

    package.json commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,21 @@
 const path = require("path");
-const webpack = require('webpack');
 const copyWebpackPlugin = require('copy-webpack-plugin');
 
-module.exports = {
-    entry : path.resolve(__dirname, 'src/main.js'),
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'ComputerVision.js',
-        library: "CV"
-    },
-    plugins: [
-        new copyWebpackPlugin([
-            {from: './public/**', to : path.resolve(__dirname, 'dist'), context: ''}
-        ])
-    ]
+module.exports = (env) => {
+    return {
+        mode: env,
+        entry : path.resolve(__dirname, 'src/main.js'),
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: 'ComputerVision.js',
+            library: "CV"
+        },
+        plugins: [
+            new copyWebpackPlugin([
+                {from: './public/**', to : path.resolve(__dirname, 'dist'), context: ''}
+            ])
+        ]
+    }
+
 };
 
